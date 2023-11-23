82b7e4785b9f683d66eb7cb18f9d5d737d5588d0
Marcus <marcus.asberg.98@gmail.com>
2023-11-23 08:45:54 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from PIL import Image
+from pytesseract import pytesseract
+import enum
+
+class Os(enum.Enum):
+    Windows = 1
+
+
+class Language(enum.Enum)
+    Eng = 'eng'
+    Jpn = 'jpn'
+
+
